d6c07a767d8c8634f3ce1af1a96397dd2cb5b345
roll <quek.ruoling@gmail.com>
2026-01-14 01:30:43 +0800

    Esbuild

---
+++ esbuild.js
@@ -0,0 +1,31 @@
+const esbuild = require("esbuild");
+const { globSync } = require("glob");
+
+const files = globSync("src/**/*.{ts,tsx}", { ignore: "src/**/types.ts" });
+
+let filePlugin = {
+    name: "files",
+    setup(build) {
+        // Mark all relative paths as external
+        build.onResolve({ filter: /(^.\/)|(^..\/)/ }, (args) => {
+            if (args.path.startsWith("./src")) {
+                return;
+            }
+            return { path: args.path, external: true };
+        });
+    },
+};
+
+esbuild
+    .build({
+        entryPoints: files,
+        bundle: true,
+        external: ["./*", "../*"],
+        outdir: "dist",
+        plugins: [
+            filePlugin,
+        ],
+        loader: { ".png": "dataurl" },
+        packages: "external",
+    })
+    .catch((e) => console.error(e.message));
